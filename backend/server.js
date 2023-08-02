b6819fc4e747ef396d8308f493a75cdfabb899fb
federico-pena <federpena22@gmail.com>
2023-08-02 18:48:21 -0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 4000
 
 app.use(require('./routes/notas.routes'))
 app.use(express.static(path.join(__dirname, '/dist')))
-console.log(__dirname)
 app.listen(PORT, () => {
 	console.log(`http://localhost:${PORT}/`)
 })
