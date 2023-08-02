6d0d8d811ece988c26b5f82bada38fec4b9024b3
federico-pena <federpena22@gmail.com>
2023-08-02 19:18:11 -0300

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(cors({ origin: '*' }))
 const PORT = process.env.PORT || 4000
 
 app.use(require('./backend/routes/notas.routes'))
-app.use('/', express.static(path.join(__dirname, '/backend', '/dist')))
+app.use(express.static(path.join(__dirname, '/backend', '/dist')))
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve('./backend', 'dist', 'index.html'))
+})
+
 app.listen(PORT, () => {
 	console.log(`http://localhost:${PORT}/`)
 })
