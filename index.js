335c8c80cad24bd03335088aa4989b02d0d8f777
federico-pena <federpena22@gmail.com>
2023-08-02 19:09:38 -0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require('./backend/server.js')
 require('./backend/database.js')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+app.use(express.json())
+app.use(cors())
+const PORT = process.env.PORT || 4000
+
+app.use(require('./backend/routes/notas.routes'))
+app.use(express.static(path.join(__dirname, '/backend/dist')))
+app.listen(PORT, () => {
+	console.log(`http://localhost:${PORT}/`)
+})
