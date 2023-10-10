25ded2f1019e47f5cdf4243c48fd1ef96503a699
federico-pena <federpena22@gmail.com>
2023-10-10 15:27:06 -0300

    page notas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'network-first',
+      manifest: {
+        name: 'Notas',
+        short_name: 'Notas',
+        theme_color: '#242424',
+        background_color: '#242424',
+        display: 'standalone',
+        scope: '/',
+        start_url: '.',
+        lang: 'es',
+        icons: [
+          {
+            src: '/assets/icons/favicon.ico',
+            type: 'image/x-icon',
+            sizes: '16x16 32x32'
+          },
+          {
+            src: '/assets/icons/icon-192.png',
+            type: 'image/png',
+            sizes: '192x192'
+          },
+          {
+            src: '/assets/icons/icon-512.png',
+            type: 'image/png',
+            sizes: '512x512'
+          },
+          {
+            src: '/assets/icons/icon-192-maskable.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable'
+          },
+          {
+            src: '/assets/icons/icon-512-maskable.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
