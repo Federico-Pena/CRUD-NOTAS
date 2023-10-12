a38cb776d68be71e42d0b25999db18c126bd692a
federico-pena <federpena22@gmail.com>
2023-10-12 02:40:55 -0300

    notas

--- frontend/src/constantes.js
+++ frontend/src/constantes.js
@@ -1,5 +1,5 @@
 export const baseUrl = import.meta.env.PROD
-  ? 'https://crud-notas.vercel.app'
+  ? 'https://tareas-finanzas.vercel.app'
   : 'http://localhost:4000'
 
 export const transacciones = ['Dia', 'Semana', 'Mes', 'Trimestre', 'Semestre', 'Año', 'Todas']
